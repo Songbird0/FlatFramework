6691ce61594bfd6cf775bef4b1f044a04e0bd4cf
Marc Moreau <contact@privateheberg.com>
2017-08-13 01:14:59 +0200

    Config Javascript loading order

--- resource/static_view/default/default.blade.php
+++ resource/static_view/default/default.blade.php
@@ -13,8 +13,9 @@
 <div id="main">
     @yield('content')
 </div>
+<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="@asset('flat.js')"></script>
 <script src="@asset('app.js')"></script>
-<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+
 </body>
 </html>
